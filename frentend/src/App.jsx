14a0cadbf99ba5aa7b83c69c7aff09da49a3a69e
Your Name <your.email@example.com>
2025-06-19 18:39:36 +0530

    update

---
+++ frentend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Chat from './components/Chat'
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
